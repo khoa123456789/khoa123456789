58c1fa4fcafc2409015998882e19e920e9a4918c
khoa123456789 <ddkhoa.20it11@vku.udn.vn>
2022-03-27 23:27:41 +0700

    aaa

---
+++ admincp/modules/config.php
@@ -0,0 +1,10 @@
+<?php
+$mysqli = new mysqli("localhost","root","","web_bg");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+?>
